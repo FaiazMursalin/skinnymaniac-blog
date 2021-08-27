4d68c327ae5accf441779d8359be8d542e5fb362
FaiazMursalin <faiazmursalin191997@gmail.com>
2021-08-28 01:20:08 +0600

    can make new blog and delete blog also added 404 pages

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,8 +1,18 @@
 import {useParams} from "react-router-dom";
 import useFetch from "./useFetch";
+import { useHistory } from "react-router";
 const BlogDetails = () => {
     const {id} =useParams();
     const {data:blog,err,isPending} = useFetch('http://localhost:8000/blogs/' + id);
+    const history = useHistory();
+    const handleDelete=()=>{
+        fetch('http://localhost:8000/blogs/'+blog.id,{
+            method:'DELETE'
+        }).then(()=>{
+            history.push('/');
+        })
+            
+    }
     
     return ( 
         <div className="blog-details">
@@ -13,6 +23,7 @@ const BlogDetails = () => {
                     <h2>{blog.title}</h2>
                     <p>- Written By {blog.author}</p>
                     <div>{blog.body}</div>
+                    <button onClick={handleDelete}>Delete</button>
                 </article>
             )}
 
